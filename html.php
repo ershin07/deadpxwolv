0707a68cc682f27ba068208cd65bc1a160237c63
ershin07 <rksimbajon@gmail.com>
2024-10-02 13:02:04 -0400

    lab 5
    creating the first php page

---
+++ html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>
+        Welcome
+       </title>
+    </head>
+
+    <body>
+        <h1>
+            Warning!!
+        </h1>
+        <p>
+            <?= var_dump($_SERVER) ?>
+        </p>
+        <p>
+            You are about to enter a extremingly painful-to-see webpage. Enter at your own risk
+        </p>
+    </body>
+<a href="welcome.html">Enter</a>
+</html>
