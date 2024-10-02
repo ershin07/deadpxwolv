753ab3279b26f95bad07700f407039740ab24829
ershin07 <rksimbajon@gmail.com>
2024-10-02 14:25:42 -0400

    form mod

---
+++ webservers/response.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This is the response page, get over with it!!!</title>
+    </head>
+    <body>
+        <p>Your Username is: <?= htmlspecialchars($_POST['username'])</p>
+        <p>Your email is: <?= htmlspecialchars($_POST['email'])?></p>
+        <p>Your age is:<?= htmlspecialchars($_POST['number'])?></p>
+        <p>mauhahaha</p>
+    </body>
+</html>
