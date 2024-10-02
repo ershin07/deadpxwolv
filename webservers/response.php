f755b3893a9d28748f18f53b909c8bff4f1be1c7
ershin07 <rksimbajon@gmail.com>
2024-10-02 14:39:24 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -2,11 +2,19 @@
 <html>
     <head>
         <title>This is the response page, get over with it!!!</title>
+
+    <?php
+        $name = htmlspecialchars($_GET['name']);
+        $email = htmlspecialchars($_GET['email']);
+        $age = htmlspecialchars($_GET['number']);
+    ?>
+
+
     </head>
     <body>
         <p>Your Username is: <?= echo $username; ?></p>
         <p>Your email is: <?= echo $email;?></p>
-        <p>Your age is:<?= echo $number;?></p>
+        <p>Your age is:<?= echo $age;?></p>
         <p>mauhahaha</p>
     </body>
 </html>
