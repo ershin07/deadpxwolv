5da5d791cbde5495c18245128130f3dc3bc02846
ershin07 <rksimbajon@gmail.com>
2024-10-02 14:47:34 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -6,14 +6,14 @@
     <?php
         $name = htmlspecialchars($_GET['username']);
         $email = htmlspecialchars($_GET['email']);
-        $age = htmlspecialchars($_GET['age']);
+        $num = htmlspecialchars($_GET['age']);
     ?>
     </head>
 
     <body>
-        <p>Your Username is: <?php echo $username; ?></p>
+        <p>Your Username is: <?php echo $name; ?></p>
         <p>Your email is: <?php echo $email; ?></p>
-        <p>Your age is:<?php echo $age; ?></p>
+        <p>Your age is:<?php echo $num; ?></p>
         <p>mauhahaha</p>
     </body>
 </html>
