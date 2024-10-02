83273338512a577b939146ef91a35410f8423e8a
ershin07 <rksimbajon@gmail.com>
2024-10-02 13:21:58 -0400

    update html.php

--- webservers/html.php
+++ webservers/html.php
@@ -14,7 +14,7 @@
             <?= var_dump($_SERVER) ?>
         </p>
         <p>
-            You are about to enter a extremingly painful-to-see webpage. Enter at your own risk
+            you about to see the start up of a .php website
         </p>
     </body>
 <a href="welcome.html">Enter</a>
