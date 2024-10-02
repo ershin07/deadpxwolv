cf99067bc649a4762bc0078976168f3cbd46771c
ershin07 <rksimbajon@gmail.com>
2024-10-02 14:34:14 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -4,7 +4,7 @@
         <title>This is the response page, get over with it!!!</title>
     </head>
     <body>
-        <p>Your Username is: <?= htmlspecialchars($_POST['username'])</p>
+        <p>Your Username is: <?= htmlspecialchars($_POST['username'])?></p>
         <p>Your email is: <?= htmlspecialchars($_POST['email'])?></p>
         <p>Your age is:<?= htmlspecialchars($_POST['number'])?></p>
         <p>mauhahaha</p>
