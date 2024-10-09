bcdb1c5f7b070fa5c8a00da719f292b0e3b9790f
ershin07 <rksimbajon@gmail.com>
2024-10-09 14:46:38 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -11,9 +11,9 @@
     </head>
 
     <body>
-        <p>Your Username is: <?php echo $name; ?></p>
-        <p>Your email is: <?php echo $email; ?></p>
-        <p>Your age is:<?php echo $num; ?></p>
+        <p>Your Username is: <?php echo htmlspecialchars($name); ?></p>
+        <p>Your email is: <?php echo htmlspecialchars($email); ?></p>
+        <p>Your age is:<?php echo htmlspecialchars($num); ?></p>
         <p>mauhahaha</p>
     </body>
 </html>
