2609473cff0fb46ff2528b29bd944bd009be3e8d
ershin07 <rksimbajon@gmail.com>
2024-10-26 22:52:02 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -29,17 +29,6 @@
             // Connection successful, show success and redirect
             echo "<script>showSuccess();</script>";
         }
-
-        // Assuming $result is your query result from the database
-        if (mysqli_num_rows($result) > 0) {
-            // Iterate through each row of the result
-            foreach ($result as $row) {
-                echo "<p>Id: {$row['id']} | Name: {$row['title']} | Developer: {$row['developer']}</p>";
-            }
-        } else {
-            echo "<p>No results found.</p>";
-        }
-        
     ?>
     <h1>
         Enter data base
