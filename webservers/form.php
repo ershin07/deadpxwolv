03b8a5a27d16d392dccf4f5cf330c6b89211f10c
ershin07 <rksimbajon@gmail.com>
2024-10-26 21:05:38 -0400

    Update form.php

--- webservers/form.php
+++ webservers/form.php
@@ -67,7 +67,7 @@
     <h1>Welcome to Game Database </h1>   
     <h2>Login </h2>
 
-        <form action="" method="GET">
+        <form action="" method="POST">
             <label for="username">Username (alphanumeric only):</label><br>
             <input type="text" id="username" name="username" 
                     pattern="[A-Za-z0-9]+" onblur="validateField()" 
