a7027cd300c1788183f84bb36f2cc31abd88db50
ershin07 <rksimbajon@gmail.com>
2024-10-27 01:25:44 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -4,6 +4,8 @@
         <title>Result</title>
     <?php  
         session_start();
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
         //initializing connection
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
             // Capture username and password from form submission
