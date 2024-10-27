40e684d270f5c40603da000a0c9e18f4b2fc8a2c
ershin07 <rksimbajon@gmail.com>
2024-10-26 20:47:01 -0400

    Update form.php

--- webservers/form.php
+++ webservers/form.php
@@ -73,11 +73,12 @@
         <form action="" method="POST">
             <label for="username">Username (alphanumeric only):</label><br>
             <input type="text" id="username" name="username" pattern="[A-Za-z0-9]+" onblur="validateField()"><br>
-            <span id="errorText" class="error">Username required</span><br><br>
+            
 
             <label for="password">Password:</label><br>
             <input type="password" id="password" name="password" required placeholder="Enter your password"><br><br>
-            <span id="errorText" class="error">Password required</span><br><br>
+
+            <span id="errorText" class="error">Username and Password required</span><br><br>
 
             <input type="submit" value="Submit">
         </form>
