5272f1d7837bc8c5c4214c0cd0558d2df90d2e7d
ershin07 <rksimbajon@gmail.com>
2024-10-26 21:37:55 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -11,7 +11,6 @@
         $conn = mysqli_connect($server, $username, $password, $database);
 
         if (!$conn) {
-            die("Connection failed: {mysqli_connect_error()}");
             echo "<p>Connection Failed</p>";
           }
           echo "<p>Connected successfully</p>";
