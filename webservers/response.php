af3d0d3963df4effb7ed9c1722d88d0d7f30a0d5
ershin07 <rksimbajon@gmail.com>
2024-10-26 21:55:52 -0400

    Update response.php

--- webservers/response.php
+++ webservers/response.php
@@ -3,6 +3,20 @@
     <head>
         <title>Result</title>
 
+    <script>
+        function errorMSG(){
+            alert('Connection Failed: $errorMessage');
+            setTimeout(function() {
+            window.location.href = 'form.html'; // Change to your actual login page
+            }, 3000);
+        }
+
+        function success(){
+            ();
+        }
+
+    </script>
+
     <?php
         $server = "localhost";
         $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
@@ -11,20 +25,15 @@
         $conn = mysqli_connect($server, $username, $password, $database);
 
         if (!$conn) {
-            // Show error popup and redirect to login after 3 seconds
-            $errorMessage = mysqli_connect_error(); // Get the connection error
-            echo "<script>
-                    alert('Connection Failed: $errorMessage');
-                    setTimeout(function() {
-                        window.location.href = 'form.html'; // Change to your actual login page
-                    }, 3000);
-                  </script>";
+            errorMSG()
+
         } else {
             // Show success popup
-            echo "<script>alert('Connected successfully');</script>";
+            echo "Connected successfully";
         }
 
     ?>
+    
     </head>
 
 </html>
